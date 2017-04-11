130633a5673c29428445bd339357c20982f07c38
ppcosys <ppcosys@gmail.com>
2017-04-11 12:38:46 +0200

    Add project files.

---
+++ CarsRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarsRental.Startup))]
+namespace CarsRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
